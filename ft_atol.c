641fd820d1d2069dc1aed04ea118e9a381de7359
jpflegha <jpflegha@student.42heilbronn.de>
2025-03-28 15:43:46 +0100

    atol

--- ft_atol.c
+++ ft_atol.c
@@ -6,7 +6,7 @@
 /*   By: jpflegha <jpflegha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 20:07:46 by jpflegha          #+#    #+#             */
-/*   Updated: 2025/03/28 15:37:48 by jpflegha         ###   ########.fr       */
+/*   Updated: 2025/03/28 15:42:31 by jpflegha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,60 +16,29 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-static void	skip_whitespace_and_check_sign(const char **str, int *sign)
-{
-	while (**str == ' ' || **str == '\t' || **str == '\n' || **str == '\r'
-		|| **str == '\f' || **str == '\v')
-	{
-		(*str)++;
-	}
-	if (**str == '-' || **str == '+')
-	{
-		if (**str == '-')
-			*sign = -1;
-		(*str)++;
-	}
-}
-
-static long	handle_conversion_and_check_overflow(const char *str, int sign)
-{
-	long	res;
-
-	res = 0;
-	while (*str >= '0' && *str <= '9')
-	{
-		if (sign == 1 && (res > LONG_MAX / 10 || (res == LONG_MAX / 10 && (*str
-						- '0') > LONG_MAX % 10)))
-		{
-			errno = ERANGE;
-			return (LONG_MAX);
-		}
-		if (sign == -1 && (res > -(LONG_MIN / 10) || (res == -(LONG_MIN / 10)
-					&& (*str - '0') > -(LONG_MIN % 10))))
-		{
-			errno = ERANGE;
-			return (LONG_MIN);
-		}
-		res = res * 10 + (*str - '0');
-		str++;
-	}
-	return (res);
-}
-
 long	ft_atol(const char *str)
 {
-	int		sign;
-	long	result;
+	unsigned long	i;
+	unsigned long	out;
+	unsigned long	j;
 
-	sign = 1;
-	skip_whitespace_and_check_sign(&str, &sign);
-	result = handle_conversion_and_check_overflow(str, sign);
-	while (*str == ' ' || *str == '\t' || *str == '\n')
-		str++;
-	if (*str != '\0')
+	j = 0;
+	out = 0;
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+				|| str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
 	{
-		errno = EINVAL;
-		return (0);
+		i++;
+		j++;
 	}
-	return (result * sign);
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		out = out * 10 + str[i] - '0';
+		i++;
+	}
+	if (str[j] == '-')
+		out = out * -1;
+	return (out);
 }
