61d9062eb0bfb55c0e1aaab726c859e5006ec4b0
jpflegha <pfleghaar.j@gmail.com>
2025-06-12 14:52:52 +0200

    changed libft.h and strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jpflegha <jpflegha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 20:41:43 by jpflegha          #+#    #+#             */
-/*   Updated: 2025/06/12 14:48:44 by jpflegha         ###   ########.fr       */
+/*   Updated: 2025/06/12 14:51:21 by jpflegha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	joined = malloc(len + 1);
 	if (!joined)
 		return (NULL);
-	ft_strcpy(joined, s1);
-	ft_strcat(joined, s2);
+	ft_strlcpy(joined, s1, ft_strlen(s1));
+	ft_strlcat(joined, s2, len);
 	return (joined);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jpflegha <jpflegha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 13:39:04 by jpflegha          #+#    #+#             */
-/*   Updated: 2025/06/12 13:44:17 by jpflegha         ###   ########.fr       */
+/*   Updated: 2025/06/12 14:52:08 by jpflegha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,7 +79,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
-char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 char	*ft_strtrim(char const *s1, char const *set);
 
