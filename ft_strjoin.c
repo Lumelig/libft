9a94473dee3d4d843542d6070896c2f115e27fa7
jpflegha <pfleghaar.j@gmail.com>
2025-06-12 14:49:32 +0200

    changed strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,46 +6,24 @@
 /*   By: jpflegha <jpflegha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 20:41:43 by jpflegha          #+#    #+#             */
-/*   Updated: 2025/05/21 00:08:02 by jpflegha         ###   ########.fr       */
+/*   Updated: 2025/06/12 14:48:44 by jpflegha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	free_and_null(char **ptr)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	if (ptr && *ptr)
-	{
-		free(*ptr);
-		*ptr = NULL;
-	}
-}
+	char	*joined;
+	size_t	len;
 
-char	*ft_strjoin(char *s1, char *s2)
-{
-	size_t	i;
-	size_t	j;
-	char	*str;
-
-	if (!s2)
-		return (s1);
-	if (!s1)
-	{
-		s1 = malloc(1);
-		if (!s1)
-			return (NULL);
-		s1[0] = '\0';
-	}
-	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (!str)
-		return (free_and_null(&s1), NULL);
-	i = -1;
-	j = 0;
-	while (s1[++i])
-		str[i] = s1[i];
-	while (s2[j])
-		str[i++] = s2[j++];
-	str[i] = '\0';
-	free_and_null(&s1);
-	return (str);
+	if (!s1 || !s2)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	joined = malloc(len + 1);
+	if (!joined)
+		return (NULL);
+	ft_strcpy(joined, s1);
+	ft_strcat(joined, s2);
+	return (joined);
 }
